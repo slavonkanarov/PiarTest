41b86cb6815c7d040c2256ab99783f346ec36740
slavonkanarov <v.kanarov@gmail.com>
2022-05-10 21:08:39 +0300

    add DATABASE_CONNECTION_URI env

--- app/config.py
+++ app/config.py
@@ -1,9 +1,11 @@
 import os
 
-user = os.environ['POSTGRES_USER']
-password = os.environ['POSTGRES_PASSWORD']
-host = os.environ['POSTGRES_HOST']
-database = os.environ['POSTGRES_DB']
-port = os.environ['POSTGRES_PORT']
-
-DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
+if "DATABASE_CONNECTION_URI" in os.environ:
+    DATABASE_CONNECTION_URI = os.environ['DATABASE_CONNECTION_URI']
+else:
+    user = os.environ['POSTGRES_USER']
+    password = os.environ['POSTGRES_PASSWORD']
+    host = os.environ['POSTGRES_HOST']
+    database = os.environ['POSTGRES_DB']
+    port = os.environ['POSTGRES_PORT']
+    DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
